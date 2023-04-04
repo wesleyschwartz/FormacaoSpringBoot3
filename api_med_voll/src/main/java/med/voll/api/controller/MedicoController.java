f401a9c8a0e7909126aa2ab64262537152c422f2
Wesley Schwartz <wesley.schwartz@compasso.com.br>
2023-04-04 17:05:12 -0300

    Aulas 1,2,3 terminadas

---
+++ api_med_voll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,25 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        medicoRepository.save(new Medico(dados));
+    }
+}
---
+++ api_med_voll/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,39 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.telefone = dados.telefone();
+        this.nome = dados.nome();
+        this.endereco = new Endereco((dados.endereco()));
+        this.crm = dados.crm();
+        this.email = dados.email();
+        this.especialidade = dados.especialidade();
+    }
+}
